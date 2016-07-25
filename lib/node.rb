1978b8aa0f4844bcc2340506323c5d95df52d6ca
DavidJaimeDesign <davidjaimesdesign@gmail.com>
2016-07-25 08:51:41 -0700

    Coninue TDD for node

--- lib/node.rb
+++ lib/node.rb
@@ -1,5 +1,23 @@
 class Node
-	def initialize(color)
+	attr_accessor :color, :vertical_node, :right_node, :left_node, :left_diagonal_node, :right_diagonal_node
+	def initialize(color, vertical_node = nil, right_node = nil, left_node = nil, left_diagonal_node = nil, right_diagonal_node = nil)
 		@color = color
+		@vertical_node = vertical_node
+		@right_node = right_node
+		@left_node = left_node
+		@left_diagonal_node = left_diagonal_node
+		@right_diagonal_node = right_diagonal_node
+	end
+
+	def add_vert(node)
+		@vertical_node = node
+	end
+
+	def add_left(node)
+		@left_node = node
+	end
+
+	def add_right(node)
+		@right_node = node
 	end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,5 +1,56 @@
 require 'node'
 
 describe Node do
-	
+	node = Node.new(color: "red")
+	context "initialize" do
+		it "initializes with a color" do
+			expect{ Node.new(color: "red") }.to_not raise_error
+		end
+
+		it "initializes with nil values for its vertical_node" do
+			expect(node.vertical_node).to eql(nil)
+		end
+
+		it "initializes with nil values for its left node" do
+			expect(node.left_node).to eql(nil)
+		end
+
+		it "initializes with nil values for itf right node" do
+			expect(node.right_node).to eql(nil)
+		end
+
+		it "initializes with nil values for upper_right_diagonal_node" do
+			expect(node.upper_right_diagonal_node).to eql(nil)
+		end
+
+		it "initializes with nil value for upper_left_diagonal_node"
+
+		it "initializes with nil values for left_diagonal_node" do
+			expect(node.left_diagonal_node).to eql(nil)
+		end
+	end
+
+	context "adding a node" do
+		connect_node = Node.new(color: "blue")
+		it "responds when color is called" do
+			expect(node.color).to eql(:color => "red")
+		end
+
+		it "can connect with a vertical_node" do
+			node.add_vert(connect_node)
+			expect(node.vertical_node).to eql(connect_node)
+			expect(node.vertical_node.color).to eql(:color => "blue")
+		end
+
+		it "can connect with a horizontal_node" do
+			node.add_left(connect_node)
+			expect(node.left_node.color).to eql(:color => "blue")
+
+			node.add_right(connect_node)
+			expect(node.right_node.color).to eql(:color => "blue")
+		end
+
+		it "can connect with a right_diagonal_node and a left_diagonal_node" do
+		end
+	end
 end
