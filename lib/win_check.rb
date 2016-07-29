33efe92da476b4722dc6a63f5b0ae70edef44fc9
DavidJaimeDesign <davidjaimesdesign@gmail.com>
2016-07-29 08:35:23 -0700

    Bisequential adding and a Win_Check class are solution alpha

---
+++ lib/win_check.rb
@@ -0,0 +1,3 @@
+class Win_Check
+	attr_accessor :board, :node
+end
---
+++ spec/win_check_spec.rb
@@ -0,0 +1,7 @@
+require 'win_check'
+describe Win_Check do
+	context "checking for win" do
+		describe "#right_check" do
+		end
+	end
+end
