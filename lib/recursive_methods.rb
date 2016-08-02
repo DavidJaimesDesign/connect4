b5b84b846d1c98e5da05a6153b3450f74c9bee2d
DavidJaimeDesign <davidjaimesdesign@gmail.com>
2016-08-02 08:48:14 -0700

    Debuging #recursive_connect

--- lib/recursive_methods.rb
+++ lib/recursive_methods.rb
@@ -2,17 +2,18 @@ require 'recursive_methods'
 require 'board'
 require 'node'
 
-def recursive_connect_nodes(node_arr[x], board)
-	return node_arr if node_arr.index == 0#return the array once you reach 0
-	node = #node at arr max
-	positions = board.sorounding_nodes
-	board.connect_nodes(node, positions)
-	recursive_connect_nodes(node_arr[x-1],board)
+class Array
+	def head_tail
+		head, *tail = *self
+		[head, tail]
+	end
 end
 
-def recursive_win_check(node_arr, board)
-	return false if node_arr.index == -1
-	node = #node at arr max
-	#hmmm we might not need this
-	recursive_win_check(node_arr[x-1],board)
+def recursive_connect_nodes(node_arr, board)
+	return node_arr if node_arr.nil? #return the array once you reach 0
+	head, tail = node_arr.head_tail
+	positions = board.sorounding_nodes(head)
+	board.connect_nodes(head, positions)
+	recursive_connect_nodes(tail,board)
 end
+
--- spec/recursive_methods_spec.rb
+++ spec/recursive_methods_spec.rb
@@ -3,9 +3,31 @@ require 'board'
 require 'node'
 
 describe "#recursive_connect_nodes" do
-	
+	it "connects nodes in an array using the board for coordinates" do
+		node0 = Node.new(color: "white")
+		node1 = Node.new(color: "white")
+		node2 = Node.new(color: "white")
+		node3 = Node.new(color: "white")
+
+		board = Board.new
+
+		board.move(node0, 3)
+		board.move(node1, 4)
+		board.move(node2, 1)
+		board.move(node3, 2)
+
+		nodes = [node0, node1, node2, node3]
+
+		recursive_connect_nodes(nodes, board)
+
+		expect(node0.right_node).to eq(node1)
+		expect(node0.left_node).to  eq(node3)
+		expect(node1.right_node).to eq(nil)
+		expect(node1.left_node).to  eq(node0)
+		expect(node2.right_node).to eq(node3)
+		expect(node2.left_node).to  eq(nil)
+		expect(node3.right_node).to eq(node0)
+		expect(node3.left_node).to  eq(node2)
+	end
 end
 
-describe "#recursive_win_check" do
-
-end
